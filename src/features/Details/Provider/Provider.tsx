bd6ce54b5e1059a952718a82c0361367c325f0a4
brandnewjinah <brandnewjinah@gmail.com>
2024-09-14 19:07:41 -0700

    feat: update providers to display multiple

--- src/features/Details/Provider/Provider.tsx
+++ src/features/Details/Provider/Provider.tsx
@@ -9,25 +9,52 @@ interface Props {
     data: ProviderResponse | null;
 }
 
-export const Provider = ({ data }: Props) => {
-    const providerPath = data?.results?.US?.flatrate[0].logo_path;
-    const providerName = data?.results?.US?.flatrate[0].provider_name;
-    const url = formatUrl(providerName ?? "");
+interface Provider {
+    provider_name: string;
+    provider_id: number;
+    logo_path: string;
+    display_priority: number;
+}
 
+const excludeProviders = [2100, 1796, 1968];
+
+export const Provider = ({ data }: Props) => {
+    const usData = data?.results?.US?.flatrate || [];
+    const filteredProviders = usData.filter(
+        (provider: Provider) => !excludeProviders.includes(provider.provider_id)
+    );
+    console.log(filteredProviders);
     return (
         <Stack border gap="1rem" padding="2rem 1rem">
             <div className={styles.header}>
                 <h3>Watch</h3>
                 <p>provided by JustWatch</p>
             </div>
-            <Link to={`/provider/${url}`} className={styles.container}>
-                {providerPath ? (
-                    <img src={`https://media.themoviedb.org/t/p/original/${providerPath}`} alt="" />
-                ) : (
-                    <div className="footnote">Not available in the US yet</div>
-                )}
-                {providerName && <p>{providerName}</p>}
-            </Link>
+            {filteredProviders &&
+                filteredProviders.length > 0 &&
+                filteredProviders.map((provider: Provider) => {
+                    const url = formatUrl(provider.provider_name ?? "");
+                    const providerPath = provider.logo_path;
+                    const providerName = provider.provider_name;
+
+                    return (
+                        <Link
+                            key={provider.provider_id}
+                            to={`/provider/${url}`}
+                            className={styles.container}
+                        >
+                            {providerPath ? (
+                                <img
+                                    src={`https://media.themoviedb.org/t/p/original/${providerPath}`}
+                                    alt=""
+                                />
+                            ) : (
+                                <div className="footnote">Not available in the US yet</div>
+                            )}
+                            {providerName && <p>{providerName}</p>}
+                        </Link>
+                    );
+                })}
         </Stack>
     );
 };
---
+++ src/helpers/constants/providers.ts
@@ -0,0 +1,14 @@
+export const excludeProviders = [
+    {
+        provider_id: 1796,
+        provider_name: "Netflix basic with Ads",
+    },
+    {
+        provider_id: 2100,
+        provider_name: "Amazon Prime Video with Ads",
+    },
+    {
+        provider_id: 1968,
+        provider_name: "Crunchyroll Amazon Channel",
+    },
+];
