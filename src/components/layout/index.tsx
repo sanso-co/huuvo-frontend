765dc6dcde6d76e028202408dce660ffd3986552
brandnewjinah <brandnewjinah@gmail.com>
2024-10-31 08:57:47 -0700

    style: hide unused components and add spacing to layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./components/Header";
 
+import styles from "./layout.module.scss";
+
 const Layout = () => {
-  return (
-    <>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <main className={styles.main}>
+                <Outlet />
+            </main>
+        </>
+    );
 };
 
 export default Layout;
