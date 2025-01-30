1a0c3786b4aaa8a80893ab2e60d8e5ad721be7d2
Jinah Lee <44072645+brandnewjinah@users.noreply.github.com>
2025-01-30 08:50:59 -0800

    chore: install google analytics (#73)

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import { RouterProvider } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { router } from "./router/router";
+import { initializeGA } from "./analytics";
 
 import "./index.scss";
 
 const queryClient = new QueryClient();
+
+initializeGA();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
--- src/services/navigation.ts
+++ src/services/navigation.ts
@@ -1,13 +1,20 @@
+import { trackPageView } from "@/analytics";
+
 class NavigationService {
     private router: any = null;
 
     setRouter(router: any) {
         this.router = router;
+
+        this.router.subscribe(({ location }: { location: Location }) => {
+            trackPageView(location.pathname);
+        });
     }
 
     navigate(path: string, options?: { state?: any }) {
         if (this.router) {
             this.router.navigate(path, options);
+            trackPageView(path);
         }
     }
 }
