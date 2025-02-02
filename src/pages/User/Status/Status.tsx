8e04ed5b77c29ef640631ebbe2752e8050df8b22
Jinah Lee <44072645+brandnewjinah@users.noreply.github.com>
2025-02-02 13:56:16 -0800

    fix: fix build err (#78)

--- src/pages/User/Status/Status.tsx
+++ src/pages/User/Status/Status.tsx
@@ -8,7 +8,6 @@ import { Sort } from "@/features/Category/Sort";
 import { ShowCard } from "@/components/feature/ShowCard";
 import { Header } from "@/components/global/Header";
 import { Spinner } from "@/components/global/Spinner";
-import { SEO } from "@/components/global/SEO";
 
 import { LeanShowType } from "@/types/show";
 
@@ -28,42 +27,36 @@ const Status = () => {
     if (error) return <div>Error: {error.message}</div>;
 
     return (
-        <>
-            <SEO
-                title={category || "Show Interactions"}
-                description={`User ${category || "interacted"} Korean dramas on K-lama.`}
-            />
-            <div className={`${styles.container} ${layout.default} ${layout.max}`}>
-                <div className={styles.header}>
-                    <Header
-                        showProfileImage={false}
-                        title={`${category} Shows`}
-                        description={`List of shows user marked ${category}`}
-                    />
-                </div>
-                <div className={styles.sort}>
-                    <Sort
-                        options={sortOptions}
-                        selected={sort}
-                        onSortSelect={(option) => setSort(option)}
-                    />
-                </div>
-                <div className={styles.content}>
-                    <InfiniteScroll
-                        dataLength={totalDocs}
-                        next={loadMore}
-                        hasMore={hasMore}
-                        loader={<Spinner />}
-                    >
-                        <div className={styles.grid}>
-                            {shows.map((show: LeanShowType) => (
-                                <ShowCard show={show} key={show.id} />
-                            ))}
-                        </div>
-                    </InfiniteScroll>
-                </div>
+        <div className={`${styles.container} ${layout.default} ${layout.max}`}>
+            <div className={styles.header}>
+                <Header
+                    showProfileImage={false}
+                    title={`${category} Shows`}
+                    description={`List of shows user marked ${category}`}
+                />
             </div>
-        </>
+            <div className={styles.sort}>
+                <Sort
+                    options={sortOptions}
+                    selected={sort}
+                    onSortSelect={(option) => setSort(option)}
+                />
+            </div>
+            <div className={styles.content}>
+                <InfiniteScroll
+                    dataLength={totalDocs}
+                    next={loadMore}
+                    hasMore={hasMore}
+                    loader={<Spinner />}
+                >
+                    <div className={styles.grid}>
+                        {shows.map((show: LeanShowType) => (
+                            <ShowCard show={show} key={show.id} />
+                        ))}
+                    </div>
+                </InfiniteScroll>
+            </div>
+        </div>
     );
 };
 
