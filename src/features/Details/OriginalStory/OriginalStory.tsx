c3624921fbba5d8f2ff7c305f1aaccd99635d8c7
brandnewjinah <brandnewjinah@gmail.com>
2025-05-14 10:47:40 -0700

    feat:originalStory

--- src/features/Details/OriginalStory/OriginalStory.tsx
+++ src/features/Details/OriginalStory/OriginalStory.tsx
@@ -2,28 +2,33 @@ import { List } from "@/components/global/List";
 
 import details from "@/assets/styles/details.module.scss";
 import layout from "@/assets/styles/layout.module.scss";
+import { useOriginal } from "@/hooks/api/details/useOriginal";
+import { formatUrl } from "@/helpers/formatUrl";
+import { Link } from "react-router-dom";
 
 interface Props {
-    data: {
-        author: {
-            name: string;
-            korean_name: string;
-        };
-        title: {
-            title: string;
-            korean_title: string;
-        };
-    };
+    id: string;
 }
 
-export const OriginalStory = ({ data }: Props) => {
+export const OriginalStory = ({ id }: Props) => {
+    const { original } = useOriginal(Number(id));
+
+    if (!original?.hasOriginalWork) {
+        return null;
+    }
+    console.log(original);
+
+    const name = formatUrl(original.author.name ?? "");
+
     return (
         <div className={`${layout.default} ${layout.max} ${details.section}`}>
             <div className={details.header}>
                 <h3>Original Story</h3>
             </div>
             <List>
-                <List.Item title={data.title.title} value={data.author.name} />
+                <Link to={`/crew/${name}/${original.author.id}`} key={original.author.id}>
+                    <List.Item title={original.title} value={original.author.name} />
+                </Link>
             </List>
         </div>
     );
